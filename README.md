fe50d18a8e8a286906d8765770c658e7a331998e
mgteixeira <mgteixeira@gmail.com>
2015-08-24 19:19:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# ideaNinja.io Build System
+
+Release 1.0
+
+This build system uses Nodejs, NPM and Gulp with the following node modules that you need to install previously: 
+ - gulp
+ - gulp-util
+ - gulp-browserify
+ - gulp-compass
+ - gulp-connect
+ - gulp-if
+ - gulp-uglify
+ - gulp-minify-html
+ - gulp-concat
+ - path
+ - gulp-save
+ - gulp-sitemap
+ - gulp-ejs
+ - gulp-imagemin
+ - imagemin-pngquant
+ - gulp-robots
+ - run-sequence
+ - gulp-clean
---
+++ gulpfile.js
@@ -0,0 +1,161 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
+    concat = require('gulp-concat');
+    path = require('path');
+    save = require('gulp-save');
+    sitemap = require('gulp-sitemap');
+    ejs = require("gulp-ejs");
+    imagemin = require('gulp-imagemin');
+    pngquant = require('imagemin-pngquant');
+    robots = require('gulp-robots');
+    runSequence = require('run-sequence');
+    var clean = require('gulp-clean');
+ 
+
+var env,
+    jsSources,
+    sassSources,
+    htmlSources,
+    outputDir,
+    sassStyle;
+
+env = 'production';
+//env = 'development';
+
+if (env==='development') {
+  outputDir = 'builds/development/';
+  sassStyle = 'expanded';
+} else {
+  outputDir = 'builds/production/';
+  sassStyle = 'compressed';
+}
+
+jsSources = [
+  'components/scripts/jquery.js',
+  'components/scripts/jqloader.js',
+  'components/scripts/transitions-bootstrap.js',
+  'components/scripts/modal-bootstrap.js',
+  'components/scripts/script.js'
+];
+
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir + '*.html'];
+
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+    .on('error', gutil.log)
+    .pipe(gulpif(env === 'production', uglify()))
+    .pipe(gulp.dest(outputDir + 'js'))
+    .pipe(connect.reload())
+});
+
+//creating robots.txt
+gulp.task('robots', function () {
+    gulp.src('builds/development/*.html')
+        .pipe(robots({ 
+          useragent: '*', 
+          out: outputDir + 'robots.txt',
+          disallow: ['/contactus.html', '/images/']
+          }))
+});
+
+//optimizes images
+gulp.task('images', function () {
+    return gulp.src('components/images/**/*.*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(outputDir +'images'));
+
+});
+
+//Enables compass with Susy and Breakpoint
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      css: outputDir + 'css',
+      image: outputDir + 'images',
+      style: sassStyle,
+      require: ['susy', 'breakpoint']
+    })
+    .on('error', gutil.log))
+//    .pipe(gulp.dest( outputDir + 'css'))
+    .pipe(connect.reload())
+});
+
+gulp.task('watch', function() {
+  gulp.watch(jsSources, ['js']);
+  gulp.watch(['components/images/*/*.*','components/images/*/*/*.*'], ['images']);
+  gulp.watch(['components/sass/*.scss', 'components/sass/*/*.scss'], ['compass']);
+  gulp.watch(outputDir +'*.html', ['sitemap', 'robots', 'html']);
+  gulp.watch('templates/*.ejs', ['ejs']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: outputDir,
+    livereload: true
+  });
+});
+
+//compiles EJS
+gulp.task('ejs', function(){
+    return gulp.src("templates/*.ejs")
+    .pipe(ejs({
+          compileDebug: true,
+          client: true        
+      }))
+  .pipe(gulp.dest(outputDir))
+}); 
+
+
+gulp.task('html', function() {
+  return gulp.src(outputDir + '*.html')
+    .pipe(gulpif(env === 'production', minifyHTML()))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+    .pipe(connect.reload())
+});
+
+//create Sitemap
+
+gulp.task('sitemap', function () {
+    return gulp.src(outputDir + '*.html')
+        .pipe(sitemap({
+            siteUrl: 'http://ideaninja.io'
+        }))
+        .pipe(gulp.dest(outputDir));
+});
+
+
+gulp.task('move', function() {
+  return gulp.src('components/scripts/countable.js')
+  .pipe(gulp.dest(outputDir+ 'js'))
+});
+
+
+gulp.task('clean', function () {
+    return gulp.src(['outputDir' + '*.*', outputDir + '**/*.*'])
+        .pipe(clean({force: true}))
+  });
+
+//gulp.task('default', ['watch', 'ejs', 'images','compass', 'move', 'robots', 'js', 'connect','html', 'sitemap']);
+
+gulp.task('default', function(callback) {
+  runSequence('clean', 
+              ['ejs', 'images','compass', 'move','js'],
+              ['robots','sitemap', 'connect','html'],
+              'watch',
+              callback);
+});
